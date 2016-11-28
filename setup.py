a6724b50f3ae4439b05dc9119319bc4ceccc5c0f
Fei Zhan <fei.zhan@jbadigital.com>
2016-11-28 12:44:06 +1100

    can be installed

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup
 import versioneer
 
-setup(name='datacycle',
-      version=versioneer.get_versions(),
+setup(name='clusteror',
+      version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
-      description='Unsupervised clustering toolkit.',
-      url='https://github.com/enfeizhan/datascience-toolkit',
+      description='Unsupervised Clustering Toolkit.',
+      url='https://github.com/enfeizhan/clusteror',
       author='Fei Zhan',
       author_email='enfeizhan@gmail.com',
       license=None,
-      packages=['datacycle'],
-      install_requires=[
-          'theano'
-      ],
-      zip_safe=False)
+      packages=['clusteror'])
