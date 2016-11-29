2e0cdab747317a93c167a2608cc6236263560afa
Fei Zhan <fei.zhan@jbadigital.com>
2016-11-30 09:04:52 +1100

    added tips example with ordinal and categorical variables

---
+++ tests_and_examples/example_tips.py
@@ -0,0 +1,81 @@
+import ipdb
+import sys
+sys.path.append('..')
+import numpy as np
+import pandas as pd
+from clusteror.core import Clusteror
+from clusteror.plot import scatter_plot_two_dim_group_data
+# from clusteror.plot import hist_plot_one_dim_group_data
+
+clusteror_valley = Clusteror.from_csv('data/tips.csv')
+# float number columns
+dat_float = clusteror_valley.raw_data.select_dtypes(include=[np.float])
+dat_float_median = dat_float.median()
+# store median
+dat_float_median.to_csv('float_median.csv')
+# float number subtracted by median and taken tanh
+clusteror_valley.cleaned_data = np.tanh(dat_float - dat_float_median)
+# Now work on ordinal and categorical columns
+# Caveat: need to retrain a model when a new value appears in these
+# columns if you want them be a clustering feature
+
+# ordinal columns can be processed similar to float number columns
+# after being numericalised. It's already in numbers in this example
+dat_ord = clusteror_valley.raw_data.loc[:, ['size']]
+ipdb.set_trace()
+dat_ord_median = dat_ord.median()
+# store median
+dat_ord_median.to_csv('ord_median.csv')
+# append to cleaned data
+clusteror_valley.cleaned_data = pd.concat(
+    [clusteror_valley.cleaned_data, np.tanh(dat_ord - dat_ord_median)],
+    axis=1
+)
+# create dummy values for categorical columns
+dat_cat = pd.get_dummies(
+    clusteror_valley.raw_data.loc[:, ['sex', 'smoker', 'day', 'time']],
+    drop_first=True
+)
+# make it equally away from 0
+dat_cat = dat_cat - 0.5
+# add to cleaned data
+clusteror_valley.cleaned_data = pd.concat(
+    [clusteror_valley.cleaned_data, dat_cat],
+    axis=1
+)
+# train neural networks
+clusteror_valley.train_sda_dim_reducer(
+    hidden_layers_sizes=[20],
+    corruption_levels=[0.1],
+    min_epochs=70,
+    improvement_threshold=0.9,
+    verbose=True
+)
+clusteror_valley.save_dim_reducer(filename='sda.pk')
+clusteror_valley.reduce_to_one_dim()
+clusteror_valley.train_valley(bins=20, contrast=0.5)
+clusteror_valley.save_valley(filename='valley.json')
+clusteror_valley.add_cluster()
+
+scatter_plot_two_dim_group_data(
+    clusteror_valley.raw_data.iloc[:, :2],
+    clusteror_valley.raw_data.cluster,
+    show=False,
+    filename='two.png'
+)
+# hist_plot_one_dim_group_data(
+#     clusteror_valley.one_dim_data,
+#     clusteror_valley.raw_data.Name,
+#     bins=50,
+#     colors=['red', 'blue', 'black'],
+#     show=False,
+#     filename='name.png'
+# )
+# hist_plot_one_dim_group_data(
+#     clusteror_valley.one_dim_data,
+#     clusteror_valley.raw_data.cluster,
+#     bins=50,
+#     colors=['red', 'blue', 'black'],
+#     show=False,
+#     filename='cluster.png'
+# )
