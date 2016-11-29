9275b7b045d8389c688808846b2b8876edd0fdfb
Fei Zhan <fei.zhan@jbadigital.com>
2016-11-30 06:58:40 +1100

    rm legacy codes

--- clusteror/__init__.py
+++ clusteror/__init__.py
@@ -1,4 +1,6 @@
 from .core import Clusteror
+from .plot import scatter_plot_two_dim_group_data
+from .plot import hist_plot_one_dim_group_data
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
